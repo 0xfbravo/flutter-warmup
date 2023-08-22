6896e5368ed7d28a7fbd44ae29f6857b888709df
Fellipe Bravo <fellipe.bravo@gmail.com>
2023-08-22 19:52:36 -0300

    Initial app structure with environment split (dev/prod)

---
+++ weather_app/android/app/src/main/kotlin/eth/0xfbravo/weatherapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package eth.oxfbravo.weatherapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ weather_app/lib/main_prod.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:WeatherApp/app.dart';
+
+void main() {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  // TODO: Any environment setup goes here.
+  runApp(const WeatherApp());
+}
