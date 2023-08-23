6869716e1f4078e5765cc241f9bda17710572591
Fellipe Bravo <fellipe.bravo@gmail.com>
2023-08-23 11:06:17 -0300

    Added main app view (tab bar) and basic dependencies

--- weather_app/lib/app.dart
+++ weather_app/lib/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weather_app/features/current_location/presentation/view.dart';
+import 'package:weather_app/features/saved_location_forecast/presentation/view.dart';
 
 class WeatherApp extends StatefulWidget {
   const WeatherApp({super.key});
@@ -10,6 +13,40 @@ class WeatherApp extends StatefulWidget {
 class _WeatherAppState extends State<WeatherApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GetMaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.cyan,
+        ),
+      ),
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Weather App'),
+            bottom: const TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.pin_drop),
+                  text: 'Current location',
+                ),
+                Tab(
+                  icon: Icon(Icons.sunny_snowing),
+                  text: 'Forecast',
+                ),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: <Widget>[
+              CurrentLocationWeatherView(),
+              SavedLocationForecastView(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
