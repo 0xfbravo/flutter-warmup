c32f226d71fa21203ec6e775ef02cc819c4a3618
Fellipe Bravo <fellipe.bravo@gmail.com>
2023-08-23 10:48:46 -0300

    Last commit fixes

--- weather_app/lib/core/setup_dependency_injection.dart
+++ weather_app/lib/core/setup_dependency_injection.dart
@@ -1,6 +1,7 @@
+import 'package:weather_app/features/current_location/dependency_injection.dart';
 import 'package:weather_app/features/saved_location_forecast/dependency_injection.dart';
 
 void setupDependencyInjection() {
-  // CurrentLocationPackage.setup();
+  CurrentLocationWeatherPackage.setup();
   SavedLocationForecastPackage.setup();
 }
