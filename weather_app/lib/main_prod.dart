74567edcf2b460b49c2a379849c32c443971f8ee
Fellipe Bravo <fellipe.bravo@gmail.com>
2023-08-26 16:39:00 -0300

    Added `dotenv_flutter` for app config
    For some reason the app isn't running on iOS from VSCode on this computer; On Xcode is running fine;

--- weather_app/lib/main_prod.dart
+++ weather_app/lib/main_prod.dart
@@ -1,5 +1,6 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 // Project imports:
 import 'package:weather_app/app.dart';
@@ -7,7 +8,14 @@ import 'package:weather_app/core/dependency_injection.dart';
 
 void main() async {
   final _ = WidgetsFlutterBinding.ensureInitialized();
-  // TODO(0xfbravo): Any environment setup goes here.
+
+  /// Keep in mind FlutterConfig doesn't obfuscate
+  /// the values, so it's not recommended to use it for sensitive data.
+  /// But it's a good option for development.
+  /// These keys should be stored on a secure storage, like for example
+  /// Transmit Security. (https://transmitsecurity.com/)
+  await dotenv.load(fileName: '.env');
+  debugPrint('.env Loaded: ${dotenv.env}');
   setupDependencyInjection();
   runApp(const WeatherApp());
 }
