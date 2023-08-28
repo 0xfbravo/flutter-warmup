b0d259cc7a714ebf2813756c1cddb90cb25f0b4f
Fellipe Bravo <fellipe.bravo@gmail.com>
2023-08-28 11:07:23 -0300

    Added `GetForecastUseCase` tests

---
+++ weather_app/test/features/forecast/domain/get_forecast_usecase_test.dart
@@ -0,0 +1,75 @@
+// 📦 Package imports:
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
+
+// 🌎 Project imports:
+import 'package:weather_app/core/dependency_injection.dart';
+import 'package:weather_app/core/domain/model/location_model.dart';
+import 'package:weather_app/features/forecast/domain/usecases/get_forecast_usecase.dart';
+
+void main() {
+  group('[Feature/Forecast] Get Forecast Use Case', () {
+    setUp(() async {
+      await dotenv.load(fileName: '.env');
+      Hive.init('./hive-tests');
+    });
+
+    tearDown(() async {
+      Hive.resetAdapters();
+      await Hive.deleteFromDisk();
+      await GetIt.I.reset();
+      dotenv.clean();
+    });
+
+    test('it should return an error', () async {
+      await setupDependencyInjection();
+      final useCase = GetForecastUseCaseImpl();
+
+      expect(
+        () => useCase(
+          location: LocationModel(
+            name: 'Abubleblé das ideias',
+            lat: double.maxFinite,
+            lon: double.maxFinite,
+          ),
+        ),
+        throwsException,
+      );
+    });
+
+    test(
+      'it should return forecast',
+      () async {
+        await setupDependencyInjection();
+        final useCase = GetForecastUseCaseImpl();
+
+        // From remote
+        await useCase(
+          location: LocationModel(
+            name: 'Monte Carlo, Monaco',
+            lat: 43.7402961,
+            lon: 7.426559,
+          ),
+        ).then((value) {
+          expect(value, isNotNull);
+          expect(value.length, greaterThan(1));
+        });
+
+        // From cache
+        await useCase(
+          location: LocationModel(
+            name: 'Monte Carlo, Monaco',
+            lat: 43.7402961,
+            lon: 7.426559,
+          ),
+        ).then((value) {
+          expect(value, isNotNull);
+          expect(value.length, greaterThan(1));
+        });
+      },
+      timeout: const Timeout(Duration(seconds: 2)),
+    );
+  });
+}
