f962c167131286d7b832d913a62acaa419581559
Fellipe Bravo <fellipe.bravo@gmail.com>
2023-08-27 22:28:03 -0300

    Added `CurrentWeatherRemoteDataSource` tests and logic

--- weather_app/lib/features/current_weather/data/remote_datasource.dart
+++ weather_app/lib/features/current_weather/data/remote_datasource.dart
@@ -1,12 +1,49 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get_it/get_it.dart';
+import 'package:weather_app/core/domain/model/saved_location_model.dart';
+import 'package:weather_app/features/current_weather/domain/model/current_weather_model.dart';
+
 abstract class CurrentWeatherRemoteDataSource {
-  Future<void> getCurrentWeather();
+  Future<CurrentWeatherModel> getCurrentWeather({
+    required SavedLocationModel savedLocationModel,
+  });
 }
 
 class CurrentWeatherRemoteDataSourceImpl
     implements CurrentWeatherRemoteDataSource {
   @override
-  Future<void> getCurrentWeather() async {
-    // TODO(0xfbravo): implement
-    return;
+  Future<CurrentWeatherModel> getCurrentWeather({
+    required SavedLocationModel savedLocationModel,
+  }) async {
+    final dio = GetIt.I<Dio>();
+    final path = dotenv.env['OPEN_WEATHER_CURRENT_WEATHER_PATH'] ?? 'unknown';
+    final response = await dio.get<dynamic>(
+      path,
+      queryParameters: {
+        'lat': savedLocationModel.lat,
+        'lon': savedLocationModel.lon,
+        'units': 'metric',
+      },
+    );
+
+    final data = response.data;
+    if (data == null) {
+      throw Exception('No data found for location: $savedLocationModel');
+    }
+
+    return CurrentWeatherModel(
+      latitude: data['coord']['lat']! as double,
+      longitude: data['coord']['lon']! as double,
+      main: data['weather'][0]['main']! as String,
+      description: data['weather'][0]['description']! as String,
+      icon: data['weather'][0]['icon']! as String,
+      temperature: data['main']['temp']! as double,
+      feelsLike: data['main']['feels_like']! as double,
+      minTemperature: data['main']['temp_min']! as double,
+      maxTemperature: data['main']['temp_max']! as double,
+      pressure: data['main']['pressure'] as double,
+      humidity: data['main']['humidity'] as double,
+    );
   }
 }
---
+++ weather_app/test/features/current_weather/data/remote_datasource_test.dart
@@ -0,0 +1,99 @@
+// 📦 Package imports:
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+// 🌎 Project imports:
+import 'package:weather_app/core/dependency_injection.dart';
+import 'package:weather_app/core/domain/model/saved_location_model.dart';
+import 'package:weather_app/features/current_weather/data/remote_datasource.dart';
+import 'package:weather_app/features/current_weather/domain/model/current_weather_model.dart';
+import 'remote_datasource_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<CurrentWeatherRemoteDataSource>()])
+void main() {
+  group('[Feature/Current Weather] Remote Datasource', () {
+    group('getCurrentWeather', () {
+      final mockLocation = SavedLocationModel(
+        name: 'Monte Carlo, Monaco',
+        lat: 43.7402961,
+        lon: 7.426559,
+      );
+
+      setUp(() async {
+        await dotenv.load(fileName: '.env');
+        Hive.init('./hive-tests');
+      });
+
+      tearDown(() async {
+        Hive.resetAdapters();
+        await Hive.deleteFromDisk();
+        await GetIt.I.reset();
+        dotenv.clean();
+      });
+
+      test('it should return an error', () {
+        final mock = MockCurrentWeatherRemoteDataSource();
+        when(mock.getCurrentWeather(savedLocationModel: mockLocation))
+            .thenThrow(Exception('Something went wrong'));
+        expect(() => mock.getCurrentWeather(savedLocationModel: mockLocation),
+            throwsException);
+      });
+
+      test('it should return a value', () {
+        final mock = MockCurrentWeatherRemoteDataSource();
+        when(mock.getCurrentWeather(savedLocationModel: mockLocation))
+            .thenAnswer(
+          (_) async => CurrentWeatherModel(
+            latitude: 0,
+            longitude: 0,
+            main: 'AAAA',
+            description: 'AAAA',
+            icon: 'AAA',
+            temperature: 0,
+            feelsLike: 0,
+            minTemperature: 0,
+            maxTemperature: 0,
+            pressure: 0,
+            humidity: 0,
+            seaLevel: 0,
+            groundLevel: 0,
+          ),
+        );
+        mock.getCurrentWeather(savedLocationModel: mockLocation).then((value) {
+          expect(value, isNotNull);
+          expect(value.description, isNotEmpty);
+        });
+      });
+
+      test('it should return do a successful request', () async {
+        await setupDependencyInjection();
+        final datasource = GetIt.I<CurrentWeatherRemoteDataSource>();
+        await datasource
+            .getCurrentWeather(savedLocationModel: mockLocation)
+            .then((value) {
+          expect(value, isNotNull);
+          expect(value.description, isNotEmpty);
+        });
+      });
+
+      test('it should return do an unsuccessful request', () async {
+        await setupDependencyInjection();
+        final datasource = GetIt.I<CurrentWeatherRemoteDataSource>();
+        expect(
+          () => datasource.getCurrentWeather(
+            savedLocationModel: SavedLocationModel(
+              name: 'Abubleblé das Ideias',
+              lat: double.maxFinite,
+              lon: double.maxFinite,
+            ),
+          ),
+          throwsException,
+        );
+      });
+    });
+  });
+}
