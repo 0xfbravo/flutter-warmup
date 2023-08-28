f8b8e4e3df4eced9c78b9c34fc342e87dbaf1126
Fellipe Bravo <fellipe.bravo@gmail.com>
2023-08-27 21:29:33 -0300

    Added `Core` use cases test

--- weather_app/test/core/data/repository_test.dart
+++ weather_app/test/core/data/repository_test.dart
@@ -40,8 +40,8 @@ void main() {
         GetIt.I.registerFactory<CoreLocalDataSource>(() => localDataSource);
         GetIt.I.registerFactory<CoreRemoteDataSource>(() => remoteDataSource);
 
-        final mock = CoreRepositoryImpl();
-        mock.getSavedLocations().then((value) => expect(value, isEmpty));
+        final repository = CoreRepositoryImpl();
+        repository.getSavedLocations().then((value) => expect(value, isEmpty));
       });
 
       test('it should return an error', () {
@@ -50,8 +50,8 @@ void main() {
         GetIt.I.registerFactory<CoreLocalDataSource>(() => localDataSource);
         GetIt.I.registerFactory<CoreRemoteDataSource>(() => remoteDataSource);
 
-        final mock = CoreRepositoryImpl();
-        expect(mock.getSavedLocations, throwsException);
+        final repository = CoreRepositoryImpl();
+        expect(repository.getSavedLocations, throwsException);
       });
 
       test('it should return a valid list', () {
@@ -63,62 +63,66 @@ void main() {
         GetIt.I.registerFactory<CoreLocalDataSource>(() => localDataSource);
         GetIt.I.registerFactory<CoreRemoteDataSource>(() => remoteDataSource);
 
-        final mock = CoreRepositoryImpl();
-        mock.getSavedLocations().then((value) => expect(value, isNotEmpty));
-      });
-    });
-  });
-
-  group('searchLocation', () {
-    const query = 'Mock Location, Mock';
-
-    setUp(() async {
-      await dotenv.load(fileName: '.env');
-      Hive.init('./hive-tests');
-    });
-
-    tearDown(() async {
-      Hive.resetAdapters();
-      await Hive.deleteFromDisk();
-      await GetIt.I.reset();
-      dotenv.clean();
-    });
-
-    test('it should return an error', () {
-      when(remoteDataSource.searchLocation(query: query))
-          .thenThrow(Exception('Something went wrong'));
-      GetIt.I.registerFactory<CoreLocalDataSource>(() => localDataSource);
-      GetIt.I.registerFactory<CoreRemoteDataSource>(() => remoteDataSource);
-
-      final mock = CoreRepositoryImpl();
-      expect(() => mock.searchLocation(query: query), throwsException);
-    });
-
-    test('it should return a value from server', () {
-      when(remoteDataSource.searchLocation(query: query)).thenAnswer(
-        (_) async => SavedLocationModel(name: 'Mock Location', lat: 0, lon: 0),
-      );
-      GetIt.I.registerFactory<CoreLocalDataSource>(() => localDataSource);
-      GetIt.I.registerFactory<CoreRemoteDataSource>(() => remoteDataSource);
-
-      final mock = CoreRepositoryImpl();
-      mock.searchLocation(query: query).then((value) {
-        expect(value, isNotNull);
-        expect(value.name.isNotEmpty, isTrue);
+        final repository = CoreRepositoryImpl();
+        repository
+            .getSavedLocations()
+            .then((value) => expect(value, isNotEmpty));
       });
     });
 
-    test('it should return a value from cache', () {
-      when(localDataSource.hasCachedLocation(query: query)).thenAnswer(
-        (_) async => SavedLocationModel(name: 'Mock Location', lat: 0, lon: 0),
-      );
-      GetIt.I.registerFactory<CoreLocalDataSource>(() => localDataSource);
-      GetIt.I.registerFactory<CoreRemoteDataSource>(() => remoteDataSource);
+    group('searchLocation', () {
+      const query = 'Mock Location, Mock';
 
-      final mock = CoreRepositoryImpl();
-      mock.searchLocation(query: query).then((value) {
-        expect(value, isNotNull);
-        expect(value.name.isNotEmpty, isTrue);
+      setUp(() async {
+        await dotenv.load(fileName: '.env');
+        Hive.init('./hive-tests');
+      });
+
+      tearDown(() async {
+        Hive.resetAdapters();
+        await Hive.deleteFromDisk();
+        await GetIt.I.reset();
+        dotenv.clean();
+      });
+
+      test('it should return an error', () {
+        when(remoteDataSource.searchLocation(query: query))
+            .thenThrow(Exception('Something went wrong'));
+        GetIt.I.registerFactory<CoreLocalDataSource>(() => localDataSource);
+        GetIt.I.registerFactory<CoreRemoteDataSource>(() => remoteDataSource);
+
+        final repository = CoreRepositoryImpl();
+        expect(() => repository.searchLocation(query: query), throwsException);
+      });
+
+      test('it should return a value from server', () {
+        when(remoteDataSource.searchLocation(query: query)).thenAnswer(
+          (_) async =>
+              SavedLocationModel(name: 'Mock Location', lat: 0, lon: 0),
+        );
+        GetIt.I.registerFactory<CoreLocalDataSource>(() => localDataSource);
+        GetIt.I.registerFactory<CoreRemoteDataSource>(() => remoteDataSource);
+
+        final repository = CoreRepositoryImpl();
+        repository.searchLocation(query: query).then((value) {
+          expect(value, isNotNull);
+          expect(value.name.isNotEmpty, isTrue);
+        });
+      });
+
+      test('it should return a value from cache', () {
+        when(localDataSource.hasCachedLocation(query: query)).thenAnswer(
+          (_) async =>
+              SavedLocationModel(name: 'Mock Location', lat: 0, lon: 0),
+        );
+        GetIt.I.registerFactory<CoreLocalDataSource>(() => localDataSource);
+        GetIt.I.registerFactory<CoreRemoteDataSource>(() => remoteDataSource);
+
+        final repository = CoreRepositoryImpl();
+        repository.searchLocation(query: query).then((value) {
+          expect(value, isNotNull);
+          expect(value.name.isNotEmpty, isTrue);
+        });
       });
     });
   });
